a92ce426967241d2df495a5aaebb238f18af6a02
Tritolium <paracrafta@gmail.com>
2023-03-03 15:08:19 +0100

    make association assignment functional

--- api/association.php
+++ api/association.php
@@ -26,6 +26,15 @@ switch($_SERVER['REQUEST_METHOD'])
         }
         break;
     case 'PUT':
+        if(isset($_GET['assign'])){
+            if(updateAssignments($_GET['api_token'], $data)){
+                http_response_code(200);
+            } else {
+                http_response_code(500);
+            }
+
+        }
+
         if(updateAssociation($_GET['api_token'], $_GET['id'], $data)){
             http_response_code(200);
         } else {
@@ -33,7 +42,11 @@ switch($_SERVER['REQUEST_METHOD'])
         }
         break;
     case 'GET':
-        if(!getAssociations($_GET['api_token'])){
+        if(isset($_GET['assign'])){
+            if(!getAssociationAssignment($_GET['api_token'])){
+                http_response_code(500);
+            }
+        } elseif (!getAssociations($_GET['api_token'])) {
             http_response_code(500);
         }
         break;
@@ -120,5 +133,146 @@ function getAssociations($api_token){
 
     response_with_data(200, $associations_array);
     return true;
+}
+
+function getAssociationAssignment($api_token)
+{
+    # cross associations with users, join assingments on member&assoc_id
+    if(!isAdmin($api_token)){
+        http_response_code(403);
+        exit();
+    }
+    
+    $database = new Database();
+    $db_conn = $database->getConnection();
+
+    $query = "SELECT assign.association_id, title, assign.member_id, taa.member_id AS assigned, assign.forename, assign.surname FROM 
+    (SELECT ta.association_id, ta.title, tm.member_id, tm.forename, tm.surname  
+    FROM tblAssociations ta 
+    JOIN tblMembers tm) AS assign
+    LEFT JOIN tblAssociationAssignments taa 
+    ON assign.association_id = taa.association_id 
+    AND assign.member_id = taa.member_id
+    ORDER BY surname, forename, association_id";
+
+    $statement = $db_conn->prepare($query);
+    
+    // wenn statement erfolgreich:
+    if($statement->execute()){
+        $success = processAssociationAssignmentStatement($statement);
+    }
+
+    return $success;
+}
+
+function processAssociationAssignmentStatement($statement)
+{
+    $assignments = array();
+    $associations = array();
+    
+    if($row = $statement->fetch(PDO::FETCH_ASSOC)){
+        $curr_member_id = intval($row['member_id']);
+
+        extract($row);
+        $association = array(
+            "Association_ID"    => $association_id,
+            "Title"             => $title,
+            "Assigned"          => (is_null($assigned)) ? false : true
+        );
+
+        array_push($associations, $association);
+
+        while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+            if($curr_member_id == $row['member_id']){
+                extract($row);
+                $association = array(
+                    "Association_ID"    => $association_id,
+                    "Title"             => $title,
+                    "Assigned"          => (is_null($assigned)) ? false : true
+                );
+                array_push($associations, $association);
+            } else {
+                $member = array(
+                    "Member_ID"     => $member_id,
+                    "Fullname"      => $forename . " " . $surname,
+                    "Associations"  => $associations
+                );
+
+                array_push($assignments, $member);
+
+                extract($row);
+                $curr_member_id = $member_id;
+                $associations = array();
+                $association = array(
+                    "Association_ID"    => $association_id,
+                    "Title"             => $title,
+                    "Assigned"          => (is_null($assigned)) ? false : true
+                );
+
+                array_push($associations, $association);
+            }
+        }
+
+        $member = array(
+            "Member_ID"     => $member_id,
+            "Fullname"      => $forename . " " . $surname,
+            "Associations"  => $associations
+        );
+
+        array_push($assignments, $member);
+
+        response_with_data(200, $assignments);
+
+        return true;
+    }
+
+    return false;
+}
+
+function updateAssignments($api_token, $assignments)
+{
+    if(!isAdmin($api_token)){
+        http_response_code(403);
+        exit();
+    }
+
+    foreach($assignments as $member => $changes){
+        processAssociationAssignments($changes, $member);
+    }
+
+    return true;
+}
+
+function processAssociationAssignments($changes, $member_id)
+{
+    foreach($changes as $association_id => $assignment){
+        $assignment ? setAssociationAssignment($association_id, $member_id) : deleteAssociationAssignment($association_id, $member_id);
+    }
+}
+
+function setAssociationAssignment($association_id, $member_id)
+{
+    $database = new Database();
+    $db_conn = $database->getConnection();
+
+    $query = "INSERT INTO tblAssociationAssignments (association_id, member_id) VALUES (:association_id, :member_id)";
+    $statement = $db_conn->prepare($query);
+    $statement->bindParam(":association_id", $association_id);
+    $statement->bindParam(":member_id", $member_id);
+
+    $statement->execute();
+}
+
+function deleteAssociationAssignment($association_id, $member_id)
+{
+    $database = new Database();
+    $db_conn = $database->getConnection();
+
+    $query = "DELETE FROM tblAssociationAssignments WHERE association_id=:association_id AND member_id=:member_id";
+    $statement = $db_conn->prepare($query);
+    $statement->bindParam(":association_id", $association_id);
+    $statement->bindParam(":member_id", $member_id);
+
+    $statement->execute();
 }
 ?>
