5fb0bc3c60f527d952a016dba3035d67721ffada
Dominik Poggel <pog@iesy.com>
2023-12-11 11:02:55 +0100

    break the execution, prevent running next part

--- api/usergroup.php
+++ api/usergroup.php
@@ -37,7 +37,7 @@ switch($_SERVER['REQUEST_METHOD'])
             } else {
                 http_response_code(500);
             }
-
+            break;
         }
         
         if(updateUsergroup($_GET['api_token'], $_GET['id'], $data)){
