46824ccd044be92e627661a0161e2b842666ccfb
Dominik Poggel <pog@iesy.com>
2024-01-29 22:55:23 +0100

    update events with POST & PUT

--- api/v0/events.php
+++ api/v0/events.php
@@ -2,25 +2,49 @@
 
 require __DIR__ . '/config/database.php';
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, OPTIONS');
 header("Content-Type: application/json; charset=UTF-8");
 
 $request = $_SERVER['REQUEST_URI'];
+// remove the /api/v0 part of the request
 $request = str_replace('/api/v0', '', $request);
+// remove the query string
+$request = explode('?', $request)[0];
+// split the request to get the id
 $request_exploded = explode('/', $request);
 
 if (isset($request_exploded[2]) && $request_exploded[2] != '') {
     $id = $request_exploded[2];
     switch ($_SERVER['REQUEST_METHOD']) {
+        case 'OPTIONS':
+            http_response_code(200);
+            break;
         case 'GET':
             getEvents($id);
             break;
+        case 'PUT':
+            updateEvent($id);
+            break;
         default:
-            http_response_code(404);
-            require __DIR__ . '/views/404.php';
+            http_response_code(405);
             break;
     }
 } else {
-    getEvents();
+    switch ($_SERVER['REQUEST_METHOD']) {
+        case 'OPTIONS':
+            http_response_code(200);
+            break;
+        case 'GET':
+            getEvents();
+            break;
+        case 'POST':
+            createEvent();
+            break;
+        default:
+            http_response_code(405);
+            break;
+    }
 }
 
 function getEvents($id = null) {
@@ -159,6 +183,67 @@ function getEvents($id = null) {
     }
 }
 
+function updateEvent($id) {
+    $database = new Database();
+    $db_conn = $database->getConnection();
 
+    $data = json_decode(file_get_contents("php://input"));
+
+    $query = "UPDATE tblEvents SET type = :type, location = :location, address = :address, category = :category, date = :date, begin = :begin, departure = :departure, leave_dep = :leave_dep, accepted = :accepted, plusone = :plusone, clothing = :clothing, usergroup_id = :usergroup_id WHERE event_id = :event_id";
+    $statement = $db_conn->prepare($query);
+
+    $statement->bindParam(":type", $data->Type);
+    $statement->bindParam(":location", $data->Location);
+    $statement->bindParam(":address", $data->Address);
+    $statement->bindParam(":category", $data->Category);
+    $statement->bindParam(":date", $data->Date);
+    $statement->bindParam(":begin", $data->Begin);
+    $statement->bindParam(":departure", $data->Departure);
+    $statement->bindParam(":leave_dep", $data->Leave_Dep);
+    $statement->bindParam(":accepted", $data->Accepted);
+    $statement->bindParam(":plusone", $data->PlusOne);
+    $statement->bindParam(":clothing", $data->Clothing);
+    $statement->bindParam(":usergroup_id", $data->Usergroup_ID);
+    $statement->bindParam(":event_id", $id);
+
+    if (!$statement->execute()) {
+        http_response_code(500);
+        exit();
+    }
+
+    response_with_data(200, $data);
+}
+
+function createEvent() {
+    $database = new Database();
+    $db_conn = $database->getConnection();
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    $query = "INSERT INTO tblEvents (type, location, address, category, date, begin, departure, leave_dep, accepted, plusone, clothing, usergroup_id) VALUES (:type, :location, :address, :category, :date, :begin, :departure, :leave_dep, :accepted, :plusone, :clothing, :usergroup_id); SELECT LAST_INSERT_ID()";
+    $statement = $db_conn->prepare($query);
+
+    $statement->bindParam(":type", $data->Type);
+    $statement->bindParam(":location", $data->Location);
+    $statement->bindParam(":address", $data->Address);
+    $statement->bindParam(":category", $data->Category);
+    $statement->bindParam(":date", $data->Date);
+    $statement->bindParam(":begin", $data->Begin);
+    $statement->bindParam(":departure", $data->Departure);
+    $statement->bindParam(":leave_dep", $data->Leave_Dep);
+    $statement->bindParam(":accepted", $data->Accepted);
+    $statement->bindValue(":plusone", $data->PlusOne ? 1 : 0);
+    $statement->bindParam(":clothing", $data->Clothing);
+    $statement->bindParam(":usergroup_id", $data->Usergroup_ID);
+
+    if (!$statement->execute()) {
+        http_response_code(500);
+        exit();
+    }
+
+    $data->Event_ID = intval($db_conn->lastInsertId());
+
+    response_with_data(201, $data);
+}
 
 ?>
